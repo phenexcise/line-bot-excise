af3cab7bb393c4b84f506a44e2341ded9ca97f36
phenexcise <91184683+phenexcise@users.noreply.github.com>
2021-09-22 16:50:26 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,6 +1,6 @@
 <?php
 
-$channelAccessToken = '/XP96K+DoalewwHBQ7tZqRnxyvkwjRYo6WXO1g2hNee+PkUbl6TRIYzEmPC2ddALuZ72gsteX96a2dNkFZliM+v8p5Ud2isyQf8GHYAYWNoQGaSmDLdlmhmmMbeCCytxBr9PkY5IzjevJ/hgeyYWsgdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$channelAccessToken = 'gXj/EI29bckDHEhF0elteszsSTU15XOF0mpDzOoMMO121eotir5KIDFkv0fGCuNtez/e1YfMzbhhVLblVr8LiKsHO8ktxf7xDwGjk6ZOszGNFX3zWePPqRxf3JmtLmJ1/CftQ/HFEmz1FXwtTUDa4wdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 
 $request = file_get_contents('php://input');   // Get request content
 
